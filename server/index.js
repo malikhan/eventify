6330307c26ceebceacdd875f86c7438c608741de
Muhammad Ali <ali558592@gmail.com>
2026-06-29 12:51:42 +0500

    add event route and model

--- server/index.js
+++ server/index.js
@@ -3,12 +3,16 @@ const dotenv = require('dotenv');
 const cors = require('cors');
 const mongoose = require('mongoose');
 const authRoutes = require('./routes/auth');
+const eventRoutes = require('./routes/events');
+const bookingRoutes = require('./routes/booking');
 
 dotenv.config();
 const app = express();
 app.use(cors());
 app.use(express.json());
 app.use('/api/auth', authRoutes);
+app.use('/api/events', eventRoutes);
+app.use('api/bookings', bookingRoutes);
 mongoose.connect(process.env.MONGO_URI, {
   
 })
